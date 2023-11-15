5f3b7b407989c98e0acbaeaabcc3fd01ad97284e
WiemMeg <wiemmessmeg@gmail.com>
2023-11-15 13:19:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import ListTask from "./Components/ListTask";
+import Addtask from "./Components/Addtask";
+import { useState } from "react";
+import { Filter } from "./Actions/Actions";
+
+function App() {
+const dispatch = useDispatch();
+
+return (
+<div className="App">
+<h2>filter by done :</h2>
+<input type="checkbox" onClick={() => dispatch(Filter())} />
+<Addtask />
+<ListTask />
+    </div>
+  );
+}
+
+export default App;
